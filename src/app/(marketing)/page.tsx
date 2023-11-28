d239b1a0dff7e0dc5ea3a1a8bb7039cae43e7db6
cjcameron92 <cjcameron92@gmail.com>
2023-11-27 21:39:08 -0500

    publish

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -5,11 +5,12 @@ import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 
-import { useState } from 'react'
 import { Inter as FontSans } from "next/font/google"
 import { Icons } from '@/components/icons'
 import { toast } from '@/components/ui/use-toast'
 import { ToastAction } from '@/components/ui/toast'
+
+import { useState, useEffect } from 'react'
 
 
 export default function IndexPage() {
@@ -18,6 +19,21 @@ export default function IndexPage() {
   const [outputText, setOutputText] = useState('') // State to hold the output text
   const [isGenerating, setGenerating] = useState(false)
 
+// Function to auto-resize text areas
+const autoResizeTextarea = (event: any) => {
+  const textarea = event.target;
+  textarea.style.height = 'auto';
+  textarea.style.height = textarea.scrollHeight + 'px';
+}
+
+// Apply auto-resize to existing content on load
+useEffect(() => {
+  const textAreas = document.querySelectorAll('textarea');
+  textAreas.forEach(textArea => {
+    textArea.style.height = 'auto';
+    textArea.style.height = textArea.scrollHeight + 'px';
+  });
+}, [outputText]); // Add outputText as a dependency
   // Function to handle the click event of the Generate button
   const asyhandleGenerate = async () => {
     // Logic to process the inputText and set the outputText
@@ -69,19 +85,7 @@ export default function IndexPage() {
           <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
           Provides in-depth, AI-powered insights into Canada&apos;s political scene. It simplifies complex policies and debates, offering users timely and accurate information on Canadian politics. Ideal for students, researchers, and enthusiasts seeking a clearer understanding of the political landscape.
           </p>
-          {/* <div className="space-x-4">
-            <Link href="/login" className={cn(buttonVariants({ size: "lg" }))}>
-              Get Started
-            </Link>
-            <Link
-              href='#'
-              target="_blank"
-              rel="noreferrer"
-              className={cn(buttonVariants({ variant: "outline", size: "lg" }))}
-            >
-              GitHub
-            </Link>
-          </div> */}
+
         </div>
       </section>
       <section
@@ -101,13 +105,14 @@ export default function IndexPage() {
   <div className="flex flex-col sm:flex-row justify-center items-center space-y-4 sm:space-y-0 sm:space-x-4">
     {/* Textarea for input */}
     <textarea
-      className="w-full sm:w-1/3 p-4 border-2 border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out focus:border-blue-500 focus:ring focus:ring-blue-200"
-      placeholder="Message poligpt"
-      value={inputText}
-      onChange={(e) => {
-        setInputText(isGenerating ? inputText : e.target.value);
-      }}
-    />
+            className="w-full sm:w-1/3 p-4 border-2 border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out focus:border-blue-500 focus:ring focus:ring-blue-200"
+            placeholder="Message poligpt"
+            value={inputText}
+            onChange={(e) => {
+              setInputText(isGenerating ? inputText : e.target.value);
+              autoResizeTextarea(e);
+            }}
+          />
     {/* Generate Button */}
     <button
       onClick={asyhandleGenerate}
@@ -120,11 +125,12 @@ export default function IndexPage() {
     </button>
     {/* Textarea for output */}
     <textarea
-      className="w-full sm:w-1/3 p-4 border-2 border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out focus:border-blue-500 focus:ring focus:ring-blue-200"
-      placeholder=""
-      value={outputText}
-      readOnly
-    />
+            className="w-full sm:w-1/3 p-4 border-2 border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out focus:border-blue-500 focus:ring focus:ring-blue-200"
+            placeholder=""
+            value={outputText}
+            readOnly
+            onChange={autoResizeTextarea}
+          />
   </div>
 </section>
 
